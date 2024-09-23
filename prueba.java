99f19a2127cadf81de902b27c3125da4da28b1b9
Alumno <carolina.aloalv@educa.jcyl.es>
2024-09-23 12:51:00 +0200

    Mostrar grupo de música favorito

--- prueba.java
+++ prueba.java
@@ -2,11 +2,11 @@ public class prueba {
 
 	public static void main(String args[]) {   
 
-		System.out.println("Alonso Alvarez");
-
 		System.out.println("19, Del 2004");
 
 		System.out.println("Espaguetis");
+
+		System.out.println("MORAT");
 
 	}
 
